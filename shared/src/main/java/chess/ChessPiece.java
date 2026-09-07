5965dc2d46f1cd4396f74a5815901c1417e69507
esenecal <55216700+esenecal@users.noreply.github.com>
2026-09-07 14:33:54 -0600

    Implement part of pieceMoves according to video.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -53,6 +53,12 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        // https://www.youtube.com/watch?v=mTtK8iRXsZo
+        ChessPiece piece = board.getPiece(myPosition);      // get the piece at the given position.
+        if (piece.getPieceType() == PieceType.BISHOP) {
+            // to do
+        }
+
         return List.of();
     }
 }
